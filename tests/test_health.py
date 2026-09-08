1d0a5734c45f634297e643d294eda3f131b4e75d
jrj6v7r5tk-ux <jrj6v7r5tk@privaterelay.appleid.com>
2026-09-08 10:10:04 +0800

    Add tests/test_health.py

---
+++ tests/test_health.py
@@ -0,0 +1,18 @@
+import pytest
+
+from conftest import assert_result
+
+
+@pytest.mark.smoke
+def test_root_returns_success(api):
+    body = assert_result(api.get("/"), code="200", message="成功")
+    assert body["data"] == "访问成功"
+
+
+@pytest.mark.smoke
+def test_root_response_contract(api):
+    body = api.get("/").json()
+    assert set(body) == {"code", "msg", "data"}
+    assert all(isinstance(body[key], str) for key in ("code", "msg", "data"))
+
+
