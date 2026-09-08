aa8053e1f48336a4fc8c7b31b82b0e1dab9e9d59
jrj6v7r5tk-ux <jrj6v7r5tk@privaterelay.appleid.com>
2026-09-08 10:10:17 +0800

    Add tests/test_authorization.py

---
+++ tests/test_authorization.py
@@ -0,0 +1,31 @@
+import pytest
+
+from conftest import assert_result
+
+
+PROTECTED_ENDPOINTS = [
+    "/notice/selectAll",
+    "/activity/selectAll",
+    "/house/selectAll",
+    "/parking/selectAll",
+    "/complaint/selectAll",
+    "/fixed/selectAll",
+    "/user/selectPage?pageNum=1&pageSize=5",
+]
+
+
+@pytest.mark.auth
+@pytest.mark.parametrize("path", PROTECTED_ENDPOINTS)
+def test_protected_endpoints_reject_missing_token(api, path):
+    body = assert_result(api.get(path), code="401", message="无效的token")
+    assert body["data"] is None
+
+
+@pytest.mark.auth
+@pytest.mark.parametrize("token", ["not-a-jwt", "abc.def.ghi"], ids=["plain-text", "jwt-shaped"])
+def test_protected_endpoint_rejects_malformed_token(api, token):
+    body = assert_result(api.get("/notice/selectAll", token=token), code="401")
+    assert body["msg"] in {"无效的token", "token验证失败，请重新登录"}
+    assert body["data"] is None
+
+
